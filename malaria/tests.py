c7dc6a6968dfdd5f7334032b14ca67ec6d569691
Irish Medina <irish.m.medina@gmail.com>
2015-08-07 23:01:16 -0500

    Added unit tests for create_revpost method

--- malaria/tests.py
+++ malaria/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
-from malaria.models import Post
-from malaria.services import delete_post_by_id, get_post_by_id
+from malaria.models import Post, RevPost
+from malaria.services import create_revpost, delete_post_by_id, get_post_by_id
 from webhub.models import Pcuser
 
 
@@ -35,18 +35,129 @@ class MalariaTests(TestCase):
         self.p2.save()
         self.p3.save()
 
+    def test_create_revpost(self):
+
+        revpost = create_revpost(self.o1,
+                                 self.p1,
+                                 self.p1.title_post,
+                                 self.p1.description_post)
+
+        self.assertIsNotNone(revpost)
+        self.assertEqual(RevPost.objects.get(pk=revpost.id), revpost)
+        self.assertEqual(revpost.owner_rev, self.o1)
+        self.assertEqual(revpost.owner_rev_post, self.p1)
+        self.assertEqual(revpost.title_post_rev, self.p1.title_post)
+        self.assertEqual(revpost.description_post_rev, self.p1.description_post)
+        self.assertEqual(revpost.title_change, True)
+        self.assertEqual(revpost.description_change, True)
+
+        revpost = create_revpost(self.o1,
+                                 self.p2,
+                                 self.p2.title_post,
+                                 self.p2.description_post)
+
+        self.assertIsNotNone(revpost)
+        self.assertEqual(RevPost.objects.get(pk=revpost.id), revpost)
+        self.assertEqual(revpost.owner_rev, self.o1)
+        self.assertEqual(revpost.owner_rev_post, self.p2)
+        self.assertEqual(revpost.title_post_rev, self.p2.title_post)
+        self.assertEqual(revpost.description_post_rev, self.p2.description_post)
+        self.assertEqual(revpost.title_change, True)
+        self.assertEqual(revpost.description_change, True)
+
+        revpost = create_revpost(self.o1,
+                                 self.p3,
+                                 self.p3.title_post,
+                                 self.p3.description_post)
+
+        self.assertIsNotNone(revpost)
+        self.assertEqual(RevPost.objects.get(pk=revpost.id), revpost)
+        self.assertEqual(revpost.owner_rev, self.o1)
+        self.assertEqual(revpost.owner_rev_post, self.p3)
+        self.assertEqual(revpost.title_post_rev, self.p3.title_post)
+        self.assertEqual(revpost.description_post_rev, self.p3.description_post)
+        self.assertEqual(revpost.title_change, True)
+        self.assertEqual(revpost.description_change, True)
+
+        revpost = create_revpost(None,
+                                 self.p1,
+                                 self.p1.title_post,
+                                 self.p1.description_post)
+
+        self.assertIsNone(revpost)
+
+        revpost = create_revpost(self.o1,
+                                 None,
+                                 self.p1.title_post,
+                                 self.p1.description_post)
+
+        self.assertIsNone(revpost)
+
+        revpost = create_revpost(self.o1,
+                                 self.p1,
+                                 None,
+                                 self.p1.description_post)
+
+        self.assertIsNone(revpost)
+
+        revpost = create_revpost(self.o1,
+                                 self.p1,
+                                 self.p1.title_post,
+                                 None)
+
+        self.assertIsNone(revpost)
+
+        revpost = create_revpost(None,
+                                 self.p1,
+                                 self.p1.title_post,
+                                 None)
+
+        self.assertIsNone(revpost)
+
+        revpost = create_revpost(self.o1,
+                                 None,
+                                 self.p1.title_post,
+                                 None)
+
+        self.assertIsNone(revpost)
+
+        revpost = create_revpost(self.o1,
+                                 None,
+                                 None,
+                                 None)
+
+        self.assertIsNone(revpost)
+
+        revpost = create_revpost(None,
+                                 None,
+                                 None,
+                                 self.p1.description_post)
+
+        self.assertIsNone(revpost)
+
+        revpost = create_revpost(None,
+                                 None,
+                                 None,
+                                 None)
+
+        self.assertIsNone(revpost)
+
     def test_delete_post_by_id(self):
 
         self.assertTrue(delete_post_by_id(self.p1.id))
         self.assertTrue(delete_post_by_id(self.p2.id))
         self.assertTrue(delete_post_by_id(self.p3.id))
 
+        self.assertFalse(delete_post_by_id(-999999))
+        self.assertFalse(delete_post_by_id(-1))
         self.assertFalse(delete_post_by_id(100))
         self.assertFalse(delete_post_by_id(200))
         self.assertFalse(delete_post_by_id(300))
         self.assertFalse(delete_post_by_id(400))
         self.assertFalse(delete_post_by_id(500))
         self.assertFalse(delete_post_by_id(600))
+        self.assertFalse(delete_post_by_id(999))
+        self.assertFalse(delete_post_by_id(999999))
 
     def test_get_post_by_id(self):
 
@@ -74,21 +185,37 @@ class MalariaTests(TestCase):
         self.assertEqual(post.title_post, self.p3.title_post)
         self.assertEqual(post.description_post, self.p3.description_post)
 
+        self.assertIsNone(get_post_by_id(-999999))
+        self.assertIsNone(get_post_by_id(-1))
         self.assertIsNone(get_post_by_id(100))
         self.assertIsNone(get_post_by_id(200))
         self.assertIsNone(get_post_by_id(300))
+        self.assertIsNone(get_post_by_id(999))
+        self.assertIsNone(get_post_by_id(999999))
 
+        self.assertNotEqual(get_post_by_id(-999999), self.p1)
+        self.assertNotEqual(get_post_by_id(-1), self.p1)
         self.assertNotEqual(get_post_by_id(100), self.p1)
         self.assertNotEqual(get_post_by_id(200), self.p1)
         self.assertNotEqual(get_post_by_id(300), self.p1)
+        self.assertNotEqual(get_post_by_id(999), self.p1)
+        self.assertNotEqual(get_post_by_id(999999), self.p1)
 
+        self.assertNotEqual(get_post_by_id(-999999), self.p2)
+        self.assertNotEqual(get_post_by_id(-1), self.p2)
         self.assertNotEqual(get_post_by_id(100), self.p2)
         self.assertNotEqual(get_post_by_id(200), self.p2)
         self.assertNotEqual(get_post_by_id(300), self.p2)
+        self.assertNotEqual(get_post_by_id(999), self.p2)
+        self.assertNotEqual(get_post_by_id(999999), self.p2)
 
+        self.assertNotEqual(get_post_by_id(-999999), self.p3)
+        self.assertNotEqual(get_post_by_id(-1), self.p3)
         self.assertNotEqual(get_post_by_id(100), self.p3)
         self.assertNotEqual(get_post_by_id(200), self.p3)
         self.assertNotEqual(get_post_by_id(300), self.p3)
+        self.assertNotEqual(get_post_by_id(999), self.p3)
+        self.assertNotEqual(get_post_by_id(999999), self.p3)
 
         self.assertNotEqual(get_post_by_id(self.p1.id), self.p2)
         self.assertNotEqual(get_post_by_id(self.p1.id), self.p3)
